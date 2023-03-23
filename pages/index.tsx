64dd73e9a12c657e913a89ac84f86994e9f90c5c
Akhmad Fauzi <akhmadfauzi0101@gmail.com>
2023-03-23 07:22:57 +0700

    path image

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
-            <Image src={"/../public/img/my_photo.png"} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width={700} height={500} loading="lazy" />
+            <Image src={"/img/my_photo.png"} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width={700} height={500} loading="lazy" />
           </div>
           <div className="col-lg-6">
             <h1 className="display-5 fw-bold lh-1 mb-3">Hi! I Am <br /> <span className='text-warning'>Akhmad Fauzi</span></h1>
